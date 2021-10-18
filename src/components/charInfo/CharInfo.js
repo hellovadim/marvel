e1957a28b13f6280afce2c5cac844acf296f238e
Vadim <hellovados@yandex.ru>
2021-10-18 21:19:22 +0300

    custom hooks

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import MarvelService from "../../services/MarvelService";
+import useMarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import Skeleton from "../skeleton/Skeleton";
@@ -9,31 +9,23 @@ import "./charInfo.scss";
 
 const CharInfo = ({ charId }) => {
   const [char, setChar] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
 
-  const marvelService = new MarvelService();
+  const { loading, error, getCharacter, clearError } = useMarvelService();
 
   useEffect(() => {
     updateChar();
   }, [charId]);
   const updateChar = () => {
+    clearError();
     if (!charId) {
       return;
     }
-    setLoading(true);
 
-    marvelService.getCharacter(charId).then(onCharLoaded).catch(onError);
+    getCharacter(charId).then(onCharLoaded);
   };
 
   const onCharLoaded = (char) => {
     setChar(char);
-    setLoading(false);
-  };
-
-  const onError = () => {
-    setLoading(false);
-    setError(true);
   };
 
   const skeleton = char || loading || error ? null : <Skeleton />;
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Spinner from "../spinner/Spinner";
-import MarvelService from "../../services/MarvelService";
+import useMarvelService from "../../services/MarvelService";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 
 import "./randomChar.scss";
@@ -10,32 +10,24 @@ import mjolnir from "../../resources/img/mjolnir.png";
 const  RandomChar = () => {
 
   const [char, setChar] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
 
 
-  const marvelService = new MarvelService();
+  const {loading, error, getCharacter, clearError} =  useMarvelService();
 
   useEffect(() => {
     updateChar()
   }, [])
 
  const onCharLoaded = (char) => {
-   setChar(char)
-   setLoading(false)
-   
+   setChar(char) 
   };
 
- const onError = () => {
-    setLoading(false)
-    setError(true)
-  }
 
 
   const updateChar = () => {
+    clearError();
     const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-    setLoading(true)
-    marvelService.getCharacter(id).then(onCharLoaded).catch(onError);
+    getCharacter(id).then(onCharLoaded)
   };
 
  
